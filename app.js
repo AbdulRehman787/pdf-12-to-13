0ca8d3aaca924687c95245f26554c821e13f6521
AbdulRehman786 <a.rehman78766@gmail.com>
2022-11-10 10:16:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,110 @@
+// var inp = prompt("Enter a number and checked this is divisble by  3")
+// if(inp %3===0){
+//     alert("This is divisible by 3")
+// }
+// else {
+//     alert("this is not divisible by 3")
+// }
+
+// var inp = prompt("Enter a number checked this is odd ya even number")
+// if(inp%2===0){
+//     alert("This is even number")
+// }else {
+//     alert("This is odd number")
+// }
+
+
+// var inp = prompt("Enter your age")
+// if(inp>=18){
+//     alert("OLd Enough");
+
+// }
+// else if(inp<18){
+//     alert("Too Young")
+// }
+// else{
+//     alert("Enter your age   ")
+// }
+
+// var inp = prompt("Enter your Name")
+// if(inp === "rehman"){
+//     alert(" Greeting ")
+// }
+// else {
+//     alert("Greeting " + inp)
+// }
+
+// var inp = +prompt("Enter a any number and checked this is divisble by 3 ")
+// if(inp % 3 === 0){
+//     alert("This is divisble by 3")
+// }
+// else{
+//     alert("This is not divisble by 3")
+// }
+
+// var inp1 = +prompt("Enter a first number you want calculate to another number");
+// var ope= prompt("Enter a any operator  ex. + ,-")
+// var inp2 = +prompt("Enter a second number you want calculate to first number");
+
+// if(ope === "+"){
+//     alert(inp1+ inp2)
+// }
+// else if(ope === "-"){
+//     alert(inp1 - inp2)
+// }
+// else if(ope === "*"){
+//     alert(inp1 * inp2)
+// }
+// else if(ope === "/"){
+//     alert(inp1 / inp2)
+// }else if(ope === "%"){
+//     alert(inp1 % inp2)
+// }
+
+// var inp = +prompt("Enter a year check this is leap year ya not ")
+// if(inp %4==0){
+//     alert("This is leap year")
+// }else{
+//     alert("This is not leap year")
+// }
+
+
+// var mainpassword = "Noor0786";
+// var password = prompt("Enter a password");
+
+// if(password === mainpassword){
+//     alert("Correct !.")
+// }else {
+//     alert("Incorrect Password")
+// }
+
+// var firstName = "Ali"
+// if(firstName ==="fahad"){
+//     document.write("Hello Fahad")
+// }
+// else{
+//     document.write("Your are not Fahad")
+// }
+
+// var hour = prompt("What Time visit in your country Enter a first number");
+// if (hour<18){
+//     alert("Good Day")
+// }else{
+//     alert("Good Evening")
+// }
+
+// var int1= prompt("Enter a first intergers")
+// var int2= prompt("Enter a second intergers")
+// if(int1===int2){
+//     alert("Both are same")
+// }
+// else{
+//     alert("Not Same")
+// }
+// var inp = +prompt("Enter a number chechk this is positive ya negative")
+// if(inp>0){
+//     alert("This is positive")
+// }
+// else if(inp<0){
+//     alert("this is negative")
+// }
